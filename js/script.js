61e46b57dc110255d3d678a8b84f5aac8f3367c9
Łukasz Spalczyński <spali89@gmail.com>
2019-06-28 22:37:36 +0200

    Delete console.log

--- js/script.js
+++ js/script.js
@@ -11,26 +11,20 @@ buttonPaper.addEventListener("click", function() {
 buttonScissors.addEventListener("click", function() {
   buttonClicked("nożyce");
 });
-
 /**
  * Function that take argument from button, then draw number to computer and compare them
  */
 function buttonClicked(argButtonName) {
   clearMessages();
-  console.log(argButtonName + " został kliknięty");
   const playerMove = argButtonName;
-  console.log("wylosowało: " + playerMove);
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log("wylosowana liczba to: " + randomNumber);
   let computerMove = getMoveName(randomNumber);
-  console.log("ruch komputera to: " + computerMove);
   displayResult(playerMove, computerMove);
 }
 /**
  * Return argument from id
  */
 function getMoveName(argMoveId) {
-  console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
   if (argMoveId == 1) {
     return "kamień";
   } else if (argMoveId == 2) {
@@ -48,12 +42,6 @@ function getMoveName(argMoveId) {
  * Match Result
  */
 function displayResult(argPlayerMove, argComputerMove) {
-  console.log(
-    "wywołano funkcję displayResults z argumentami: " +
-      argPlayerMove +
-      ", " +
-      argComputerMove
-  );
   if (argPlayerMove == "papier" && argComputerMove == "kamień") {
     printMessage("Wygrywasz!");
   } else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
